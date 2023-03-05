e6a5ea2e51620cc6430eb2e615fbe9cdf49ea048
bene-tan <125938524+bene-tan@users.noreply.github.com>
2023-03-05 17:45:23 +0800

    Add files via upload

---
+++ PSQ.js
@@ -0,0 +1,41 @@
+function changeTextFormat() {
+var valueTwo = loader.engine.document.getElementById(104009226).getProperty('value.value');
+loader.engine.document.getElementById(104009226).setValue(({"value": valueTwo.toUpperCase()})); 
+}
+window.onchange = changeTextFormat;
+
+function changeTextFormat() {
+var valueTwo = loader.engine.document.getElementById(104009250).getProperty('value.value');
+loader.engine.document.getElementById(104009250).setValue(({"value": valueTwo.toUpperCase()})); 
+}
+window.onchange = changeTextFormat;
+
+function changeTextFormat() {
+var valueTwo = loader.engine.document.getElementById(105388462).getProperty('value.value');
+loader.engine.document.getElementById(105388462).setValue(({"value": valueTwo.toUpperCase()})); 
+}
+window.onchange = changeTextFormat;
+
+function changeTextFormat() {
+var valueTwo = loader.engine.document.getElementById(48892431).getProperty('value.value');
+loader.engine.document.getElementById(48892431).setValue(({"value": valueTwo.toUpperCase()})); 
+}
+window.onchange = changeTextFormat;
+
+function changeTextFormat() {
+var valueTwo = loader.engine.document.getElementById(103942400).getProperty('value.value');
+loader.engine.document.getElementById(103942400).setValue(({"value": valueTwo.toUpperCase()})); 
+}
+window.onchange = changeTextFormat;
+
+function changeTextFormat() {
+var valueTwo = loader.engine.document.getElementById(103942415).getProperty('value.value');
+loader.engine.document.getElementById(103942415).setValue(({"value": valueTwo.toUpperCase()})); 
+}
+window.onchange = changeTextFormat;
+
+function changeTextFormat() {
+var valueTwo = loader.engine.document.getElementById(103942420).getProperty('value.value');
+loader.engine.document.getElementById(103942420).setValue(({"value": valueTwo.toUpperCase()})); 
+}
+window.onchange = changeTextFormat;
