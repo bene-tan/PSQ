d81a2f1a31d31c1beb78eebef6c103efb262685e
bene-tan <125938524+bene-tan@users.noreply.github.com>
2023-03-05 17:53:39 +0800

    Update PSQ.js

--- PSQ.js
+++ PSQ.js
@@ -1,41 +1,41 @@
 function changeTextFormat() {
-var valueTwo = loader.engine.document.getElementById(104009226).getProperty('value.value');
-loader.engine.document.getElementById(104009226).setValue(({"value": valueTwo.toUpperCase()})); 
+var valueTwo = loader.engine.document.getElementById(106002984).getProperty('value.value');
+loader.engine.document.getElementById(106002984).setValue(({"value": valueTwo.toUpperCase()})); 
 }
 window.onchange = changeTextFormat;
 
 function changeTextFormat() {
-var valueTwo = loader.engine.document.getElementById(104009250).getProperty('value.value');
-loader.engine.document.getElementById(104009250).setValue(({"value": valueTwo.toUpperCase()})); 
+var valueTwo = loader.engine.document.getElementById(106002989).getProperty('value.value');
+loader.engine.document.getElementById(106002989).setValue(({"value": valueTwo.toUpperCase()})); 
 }
 window.onchange = changeTextFormat;
 
 function changeTextFormat() {
-var valueTwo = loader.engine.document.getElementById(105388462).getProperty('value.value');
-loader.engine.document.getElementById(105388462).setValue(({"value": valueTwo.toUpperCase()})); 
+var valueTwo = loader.engine.document.getElementById(106002990).getProperty('value.value');
+loader.engine.document.getElementById(106002990).setValue(({"value": valueTwo.toUpperCase()})); 
 }
 window.onchange = changeTextFormat;
 
 function changeTextFormat() {
-var valueTwo = loader.engine.document.getElementById(48892431).getProperty('value.value');
-loader.engine.document.getElementById(48892431).setValue(({"value": valueTwo.toUpperCase()})); 
+var valueTwo = loader.engine.document.getElementById(106002995).getProperty('value.value');
+loader.engine.document.getElementById(106002995).setValue(({"value": valueTwo.toUpperCase()})); 
 }
 window.onchange = changeTextFormat;
 
 function changeTextFormat() {
-var valueTwo = loader.engine.document.getElementById(103942400).getProperty('value.value');
-loader.engine.document.getElementById(103942400).setValue(({"value": valueTwo.toUpperCase()})); 
+var valueTwo = loader.engine.document.getElementById(106003124).getProperty('value.value');
+loader.engine.document.getElementById(106003124).setValue(({"value": valueTwo.toUpperCase()})); 
 }
 window.onchange = changeTextFormat;
 
 function changeTextFormat() {
-var valueTwo = loader.engine.document.getElementById(103942415).getProperty('value.value');
-loader.engine.document.getElementById(103942415).setValue(({"value": valueTwo.toUpperCase()})); 
+var valueTwo = loader.engine.document.getElementById(106003163).getProperty('value.value');
+loader.engine.document.getElementById(106003163).setValue(({"value": valueTwo.toUpperCase()})); 
 }
 window.onchange = changeTextFormat;
 
 function changeTextFormat() {
-var valueTwo = loader.engine.document.getElementById(103942420).getProperty('value.value');
-loader.engine.document.getElementById(103942420).setValue(({"value": valueTwo.toUpperCase()})); 
+var valueTwo = loader.engine.document.getElementById(106003165).getProperty('value.value');
+loader.engine.document.getElementById(106003165).setValue(({"value": valueTwo.toUpperCase()})); 
 }
 window.onchange = changeTextFormat;
